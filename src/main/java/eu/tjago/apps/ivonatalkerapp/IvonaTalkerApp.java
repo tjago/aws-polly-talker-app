2f641596a080e0061cc749afdd9750ff2e532b7f
Tomasz Jagodowski <munduss@gmail.com>
2015-12-24 02:04:11 +0100

    added main class

---
+++ src/main/java/eu/tjago/apps/ivonatalkerapp/IvonaTalkerApp.java
@@ -0,0 +1,10 @@
+package eu.tjago.apps.ivonatalkerapp;
+
+/**
+ * Created by Tomasz on 2015-12-24.
+ */
+public class IvonaTalkerApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Ivona, we will have a talk, when you plug the API");
+    }
+}
